19f41846609ea9024487e028408903aa8567d4fb
ellisvelandia <eyis619@gmail.com>
2022-09-08 18:02:45 -0500

    toon

---
+++ .history/vite.config_20220908180030.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "https://<Ellisvelandia>.github.io/<react-rick-and-morty>/", 
+  react-rick-and-morty
+  plugins: [react()]
+})
