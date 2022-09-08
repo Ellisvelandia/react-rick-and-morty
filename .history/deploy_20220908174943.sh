c318151b290c11d39c237c67ae5ae83bb13072f5
ellisvelandia <eyis619@gmail.com>
2022-09-08 17:52:41 -0500

    toon

---
+++ .history/deploy_20220908174943.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+npm run build
+
+cd dist
+
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+cd -
--- .history/vite.config_20220908174714.js
+++ .history/vite.config_20220908174714.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://<Ellisvelandia>.github.io/<react-rick-and-morty>/", 
+  base: "https://<USERNAME>.github.io/<REPO>/", 
   plugins: [react()]
 })
